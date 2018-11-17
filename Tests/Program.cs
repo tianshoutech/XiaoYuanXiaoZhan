cbbf416589fd7109298551140be62f85d3c6c628
gebizhuifengren <zhangzhuang_dev@outlook.com>
2018-11-17 23:28:31 +0800

    Revert "1 提交发送邮件相关的代码"
    This reverts commit 5e24b489d42cebd781c7531338e9a2037c406217.

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using XiaoYuanXiaoZhan.Utils.Implement;
-using XiaoYuanXiaoZhan.Utils.Model;
 
 namespace Tests
 {
@@ -9,26 +7,7 @@ namespace Tests
     {
         static void Main(string[] args)
         {
-            //ExcelTest.GenerateExcel();
-
-            var emailManger = new EmailManager();
-            var emailModel = new EmailModel();
-            emailModel.Subject = "测试";
-            emailModel.HtmlBody = "<p>测试</p>";
-            emailModel.TextBody = "测试";
-            emailModel.MailToAddress = "gebizhuifengren@aliyun.com";
-            emailModel.MailToUser = "戈壁追风人";
-            emailModel.Attachments = new List<string>();
-            emailModel.Attachments.Add(@"C:\Users\stron\Desktop\ddd.txt");
-            emailModel.Attachments.Add(@"C:\Users\stron\Desktop\bbb.xlsx");
-
-            var list = new List<EmailModel>();
-            for (int i = 0; i < 30; i++)
-            {
-                list.Add(emailModel);
-            }
-
-            emailManger.SendEmailBatch(list).Wait();
+            ExcelTest.GenerateExcel();
 
             Console.WriteLine("生成完毕");
             Console.ReadKey();
